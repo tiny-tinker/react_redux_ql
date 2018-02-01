afc1686bcb7cee802cc54962456dc44dc19739de
Mark <mwbdevelopment@gmail.com>
2018-02-01 03:35:17 -0500

    benefit section

--- src/app/components/BenefitList/BenefitSelection.js
+++ src/app/components/BenefitList/BenefitSelection.js
@@ -2,15 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import * as styles from './BenefitSelection.css';
 
-const BenefitSelection = function BenefitSelection() {
+const BenefitSelection = function BenefitSelection({ benefit, handleClick }) {
   const container = ['row', styles.container].join(' ');
+  const imgSrc = require(`./../../../assets/images/${benefit.image}`);
+
   return (
-    <div className={container}>
+    <a onClick={() => handleClick(benefit)} className={container}>
       <div className={styles.container_img}>
-        <img src={require('./../../../assets/images/')}
+        <img src={imgSrc} />
       </div>
-    </div>
+      <div>
+        <div className={styles.title}>
+          { benefit.title }
+        </div>
+        <p className={styles.sub_title}>
+          { benefit.subtitle }
+        </p>
+      </div>
+    </a>
   );
+};
+
+BenefitSelection.propTypes = {
+  benefit: PropTypes.object,
+  benefitClick: PropTypes.func,
 };
 
 export default BenefitSelection;
